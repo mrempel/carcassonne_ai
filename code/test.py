32cb43e2347597429be4d0fb9cc4611d34e6dd86
mrempel <rempel.mark@gmail.com>
2019-03-12 21:20:10 -0400

    This commit introduces a shell in the test.py file to randomly select tiles from a bag of tiles (assuming three players). It includes place holders for each player to decide where to place their selected tile and whether or not to place a meeple on the tile.

--- code/test.py
+++ code/test.py
@@ -1,6 +1,10 @@
 import numpy as np
 import csv
 
+#------------------------------------------------------------------------------
+# Class definitions
+
+# Create a class for a tile.
 class tileClass:
 
     def setData(self, attributes):
@@ -13,17 +17,58 @@ class tileClass:
     def display(self):
         print(self.north)
 
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+# Setup the game
+
 # Create an empty list of tiles
 tiles = []
 
+# Read the set of tiles
 with open('../tiles/tile_descriptions.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     next(readCSV)
     for row in readCSV:
-        print(row)
 
         tile = tileClass()
         tile.setData(row)
         tiles.append(tile)
 
-tiles[20].display()
+#------------------------------------------------------------------------------
+# Play the game
+
+playersTiles = [[], [], []]
+
+turn = 0
+while tiles.__len__() > 0:
+
+    player = 0
+    while player < playersTiles.__len__():  
+        
+        # Player one draws a tile, makes a decision as to where to place it, and
+        # makes a decision if to place a meeple or not.
+
+        # Draw a tile
+        drawnTile = np.random.choice(tiles.__len__())
+        playersTiles[player].append(tiles[drawnTile])
+        tiles.pop(drawnTile)
+
+        # Decide where to place the tile
+
+        # Decide if to place a meeple or not
+
+        # If there are no tiles remaining, but not all the players have selected
+        # a tile this turn, then break and the game is done; else, move to the next
+        # player
+        if tiles.__len__() == 0:
+            break
+        else:
+            player = player + 1
+
+    # Increment the turn counter
+    turn = turn + 1
+    
+
+
+
